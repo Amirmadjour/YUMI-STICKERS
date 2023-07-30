91536f43531e539eeeeeb4e8f5cfa768662ac730
amirmadjour <amirmadjour@kali.KALI>
2023-07-30 11:01:14 +0100

    need some adjusment in the hero section for responsiveness

--- function.js
+++ function.js
@@ -1,66 +1,69 @@
 let image_number = JSON.parse(localStorage.getItem("img_num"));
-let img = document.querySelectorAll('.bookmark-img');
+let img = document.querySelectorAll(".bookmark-img");
 
-image_number === null && (image_number
- = 0);  
+image_number === null && (image_number = 0);
 img[image_number].classList.add("grow");
 
-let left_btn = document.querySelector('.left-switcher');
-let right_btn = document.querySelector('.right-switcher');
+let left_btn = document.querySelector(".left-switcher");
+let right_btn = document.querySelector(".right-switcher");
 
-left_btn.addEventListener("click", () =>
-{
-  img[image_number
-].classList.remove("grow");
-  image_number
- = image_number
- === 0 ? 7 : image_number
- - 1;
-  img[image_number
-].classList.add("grow");
-  localStorage.setItem("img_num", JSON.stringify(image_number
-  ));
-})
+left_btn.addEventListener("click", () => {
+  img[image_number].classList.remove("grow");
+  image_number = image_number === 0 ? 7 : image_number - 1;
+  img[image_number].classList.add("grow");
+  localStorage.setItem("img_num", JSON.stringify(image_number));
+});
 
-right_btn.addEventListener("click", () =>
-{
-  img[image_number
-].classList.remove("grow");
-  image_number
- = (image_number
-   + 1) % 8;
-  img[image_number
-].classList.add("grow");
-  localStorage.setItem("img_num", JSON.stringify(image_number
-  ));
-})
+right_btn.addEventListener("click", () => {
+  img[image_number].classList.remove("grow");
+  image_number = (image_number + 1) % 8;
+  img[image_number].classList.add("grow");
+  localStorage.setItem("img_num", JSON.stringify(image_number));
+});
 
-const sections = document.querySelectorAll('section')
-const nav_links = document.querySelectorAll('.nv-btn')
+const sections = document.querySelectorAll("section");
+const nav_links = document.querySelectorAll(".nv-btn");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
-    if(top >= offset && top < offset + height) {
-      for(let i = 0; i < nav_links.length; i++) {
+    if (top >= offset && top < offset + height) {
+      for (let i = 0; i < nav_links.length; i++) {
         if (nav_links[i].getAttribute("href") === "#" + id) {
-          nav_links[i].classList.add('active');
+          nav_links[i].classList.add("active");
         } else {
-          nav_links[i].classList.remove('active');
+          nav_links[i].classList.remove("active");
         }
       }
     }
-  })
+  });
+};
+
+function replaceherodescription() {
+
+  const main_header_description_1 = document.getElementById("hero-description-1");
+  const main_header_description_2 = document.getElementById("hero-description-2");
+  const hero_btn = document.getElementById("hero-order-btn");
+  const hero = document.getElementById("hero");
+  const hero_container = document.getElementsByClassName("hero-container");
+
+  if (window.innerWidth < 1024) {
+    hero.appendChild(main_header_description_1);
+    hero.appendChild(main_header_description_2);
+    hero.appendChild(hero_btn);
+    console.log("This should work!!");
+  }
+  else {
+    hero_container[0].appendChild(main_header_description_1);
+    hero_container[0].appendChild(main_header_description_2);
+    hero_container[0].appendChild(hero_btn);
+  }
 }
 
-const main_header_description = herocontainer.querySelectorAll("p");
+replaceherodescription();
 
-if(window.innerWidth < 1024) {
-  main_header_description.remove();
-
-  console.log("this should worked!!");
-}
+window.addEventListener("resize", replaceherodescription);
